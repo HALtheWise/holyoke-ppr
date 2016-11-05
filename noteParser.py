7f2c86806bc4aa327c80096eab2291b05cfa64df
Eric Miller <eric@legoaces.org>
2016-11-05 01:26:19 -0400

    Added ability for noteParser to read MIDI data

--- noteParser.py
+++ noteParser.py
@@ -5,6 +5,10 @@ that will be read from a .midi file, with eventual extensions to audio files hop
 Maintainer: Eric
 """
 
+from mido import MidiFile
+
+import math
+
 numNotes = 8
 
 class TimeSlice():
@@ -48,6 +52,73 @@ class Song():
 	__repr__ = __str__
 
 
+def getMIDISong(filename='test data/birthday-single.mid'):
+	""" Code adapted from 
+	https://github.com/olemb/mido/blob/master/examples/midifiles/print_midi_file.py"""
+
+	midi = MidiFile(filename)
+
+	global track
+	track = getMainTrack(midi)
+	
+	notes = []
+
+	elapsedtime = 0
+
+	for i, msg in enumerate(track):
+		elapsedtime += msg.time
+	
+		if msg.type == 'note_on':
+			notes.append((elapsedtime, msg.note, findNoteDuration(track, msg.note, i+1)))
+
+	return notes
+
+def notesToSong(notes, dt = 0.25, midispeed = 96*4, baseID = 60):
+	def convertNoteID(noteID):
+		result = noteID - baseID
+		if (result < 0):
+			result = 0
+		if (result >= numNotes):
+			result = numNotes - 1
+		return result
+
+	def convertTime(time):
+		return int(math.ceil(time/midispeed/dt))
+
+	duration = convertTime(notes[len(notes)-1][0] + notes[len(notes)-1][2]) 
+
+	# Initialize timeslices
+	timeslices = [TimeSlice() for _ in xrange(duration)]
+
+	# Populate timeslices
+	for note in notes:
+		for i in xrange(convertTime(note[0]), convertTime(note[0]+note[2])):
+			timeslices[i].setNote(convertNoteID(note[1]), True)
+
+	return Song(timeslices)
+
+
+def findNoteDuration(track, noteID, startpos):
+	""" Finds the duration until note_off is called on noteID, starting search at startpos """
+	elapsedtime = 0
+	i = startpos
+	while i < len(track):
+		msg = track[i]
+		elapsedtime += msg.time
+
+		if msg.type == 'note_off' and msg.note == noteID:
+			return elapsedtime
+
+		i += 1
+
+def printTrack(track):
+	print '=== Track length {}\n'.format(len(track))
+	for message in track:
+		print '  {!r}\n'.format(message)
+
+def getMainTrack(midi):
+	return max(midi.tracks, key=len)
+
 def getTestSong():
 	song = Song([
 		TimeSlice(),
@@ -64,4 +135,4 @@ def getTestSong():
 	return song
 
 if __name__ == '__main__':
-	print getTestSong()
+	print notesToSong(getMIDISong())
